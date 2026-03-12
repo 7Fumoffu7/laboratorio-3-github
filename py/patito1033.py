480936391a22a65875b6f8c28d8dde7469bb9d44
Josue <vegetjoshua21@gmail.com>
2026-03-12 19:01:03 -0400

    problema py resuelto primos

---
+++ py/patito1033.py
@@ -0,0 +1,26 @@
+import sys
+
+input = sys.stdin.readline
+MAX = 10_000_000
+
+prime = bytearray(b'\x01') * (MAX + 1)
+prime[0] = prime[1] = 0
+
+limit = int(MAX ** 0.5) + 1
+
+for i in range(2, limit):
+    if prime[i]:
+        prime[i*i:MAX+1:i] = b'\x00' * ((MAX - i*i)//i + 1)
+
+pref = [0] * (MAX + 1)
+count = 0
+for i in range(MAX + 1):
+    if prime[i]:
+        count += 1
+    pref[i] = count
+t = int(input())
+for _ in range(t):
+    a, b = map(int, input().split())
+    if a < 1:
+        a = 1
+    print(pref[b] - pref[a - 1])
